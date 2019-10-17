a604bbb12ce749fa43c64fe83ac2ddd8e3d6aeaf
Chryen <squirtlexomg@yahoo.com>
2019-10-17 20:43:48 +0000

    Submitted studies

---
+++ studies/datatypes.js
@@ -0,0 +1,23 @@
+/*
+ * DATA TYPES:
+ * 
+ * Any type of value can be considered a data-type.  Datatypes can be separated into two groups:
+ * Simple/Primitive and Complex.
+ * 
+ * Simple data-types are atomic/immmutable.  They do not hold, collect or aggregate other values.  
+ * The operations on simple values return new simple values and do not alter the original value.
+ * 
+ * Complex data-types aggregate other values and therefore are of indefinite size.  When assigning 
+ * or passing, complete data-types are passed by reference.
+ */
+
+/*
+ * SIMPLE/PRIMITIVE:
+ * - Number : Numeric data
+ * - String : Character data
+ * - Boolean : true of false
+ * - NaN : Not a Number (typeof NaN: // => 'number')
+ * - undefined : no-value, incept, uninitialized.
+ * - null : no-value, nullified by a programmer
+ */
+
